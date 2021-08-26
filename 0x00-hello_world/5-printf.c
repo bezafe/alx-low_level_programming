c113eb4d8463c482473664464d334d97c7da12a8
bezafe <bezafelekework@gmail.com>
2021-08-26 04:22:32 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art;
+ * return: 0 when exited
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+}
