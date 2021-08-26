eca818e0c24198fe77b1b8c1787e113b1384967a
bezafe <bezafelekework@gmail.com>
2021-08-26 03:39:56 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
