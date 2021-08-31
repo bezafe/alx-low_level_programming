af811dd83c0280fab7b38b682037cbb65772fb3d
bezafe <bezafelekework@gmail.com>
2021-08-31 06:01:57 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char holberton[] = "Holberton";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
